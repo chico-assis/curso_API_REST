454d402cea23c105a0a369d89fbb6a48d64d1764
chico-assis <114764855+chico-assis@users.noreply.github.com>
2022-09-30 17:52:23 +0100

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'H38PEJRSMp',
+      database: 'barriga',
+    },
+    migrations: { directory: 'src/migrations' },
+    seeds: { directory: 'src/seeds' },
+  },
+};
---
+++ src/app.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const consign = require('consign');
+const knex = require('knex');
+
+const knexfile = require('../knexfile');
+
+// TODO criar chaveamento dinamico
+app.db = knex(knexfile.test);
+
+consign({ cwd: 'src', verbose: false })
+  .include('./config/passport.js')
+  .include('./config/middlewares.js')
+  .then('./services')
+  .then('./routes')
+  .then('./config/router.js')
+  .into(app);
+
+app.get('/', (req, res) => {
+  res.status(200).send();
+});
+
+app.use((err, req, res, next) => {
+  const { name, message, stack } = err;
+  if (name === 'ValidationError') res.status(400).json({ error: message });
+  else if (name === 'RecursoIndevidoError') res.status(403).json({ error: message });
+  else {
+    console.log(message);
+    res.status(500).json({ name, message, stack });
+  }
+  next(err);
+});
+
+module.exports = app;
